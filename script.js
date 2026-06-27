7193db2858e1e1d996d02483c444e10a5c7109aa
minsong2ya <minsong2ya@gmail.com>
2026-06-27 20:50:46 +0900

    첫 화면 실제 공부 인원 표시

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import {
+  getFirestore,
+  doc,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2z_oPZdwKT1w205xHs5dRVP8_AIzCC78",
+  authDomain: "library-study-b9678.firebaseapp.com",
+  projectId: "library-study-b9678",
+  storageBucket: "library-study-b9678.firebasestorage.app",
+  messagingSenderId: "93674680499",
+  appId: "1:93674680499:web:51865432e58e3f23c740c6"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const onlineCountRef = doc(db, "studyStatus", "onlineCount");
 // 개강일 설정
 const startDate = new Date("2026-09-01");
 
@@ -25,9 +44,12 @@ function updateStudyingCount() {
   const studyingText = document.getElementById("studyingText");
 
   // 임시 랜덤 인원수
-  const count = Math.floor(Math.random() * 35) + 12;
+ onSnapshot(onlineCountRef, function (docSnap) {
+  const data = docSnap.data();
+  const count = data?.count ?? 0;
 
   studyingText.textContent = `${count}명 공부 중`;
+});
 }
 
 function enterSite() {
